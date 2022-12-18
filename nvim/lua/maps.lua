30a2aa533a2643cce2f87bb20cf60e773d83cd44
Stoyko Tolev <stoyko@Stoykos-MacBook-Pro.local>
2022-12-18 17:18:31 +0200

    Feat: Add comments to the keymap file.

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -1,44 +1,60 @@
-local keymap = vim.keymap.set
+local map = vim.keymap.set
 local o = { silent = true }
+
+-- set leader to space --
 vim.g.mapleader = " "
 
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   command_mode = "c",
 
-keymap('n', '+', '<C-a>')
-keymap('n', '-', '<C-x>')
+-- move line up/down
+map("n", "<leader>j", "ddp", { silent = true })
+map("n", "<leader>k", "ddkP", { silent = true })
 
 
-keymap('n', 'dw', 'vb"_d')
-keymap('n', '<C-a>', 'gg<S-v>G')
-keymap('n', '<C-d>', '<C-d>zz')
-keymap('n', '<C-u>', '<C-u>zz')
-keymap('n', 'n', 'nzz')
-keymap('n', 'N', 'Nzz')
+map('n', '+', '<C-a>')
+map('n', '-', '<C-x>')
 
-keymap('n', 'te', ':tabedit<Return>', { silent = true })
-keymap('n', 'ss', ':split<Return><C-w>w', { silent = true })
-keymap('n', 'sv', ':vsplit<Return><C-w>w', { silent = true })
 
-keymap('n', '<Space>', '<C-w>w')
-keymap('', 's<left>', '<C-w>h')
-keymap('', 's<up>', '<C-w>k')
-keymap('', 's<down>', '<C-w>j')
-keymap('n', 'yw', 'yiw', o) -- yank a word from anywhere
-keymap('', 's<right>', '<C-w>l')
-keymap('', 'sh', '<C-w>h')
-keymap('', 'sk', '<C-w>k')
-keymap('', 'sj', '<C-w>j')
-keymap('', 'sl', '<C-w>l')
+map('n', 'dw', 'vb"_d')
+map('n', '<C-a>', 'gg<S-v>G')
+map('n', '<C-d>', '<C-d>zz')
+map('n', '<C-u>', '<C-u>zz')
+map('n', 'n', 'nzz')
+map('n', 'N', 'Nzz')
 
-keymap('n', '<C-w><left>', '<C-w><')
-keymap('n', '<C-w><right>', '<C-w>>')
-keymap('n', '<C-w><up>', '<C-w>+')
-keymap('n', '<C-w><down>', '<C-w>-')
+-- tabs --
+map('n', 'te', ':tabedit<Return>', { silent = true }) -- opens a new tab
+map('n', 'tc', ':tabclose<Return>', { silent = true }) -- closes the tab but keeps it in the buffer
 
-keymap('n', '<C-/>', ':s/^/#<CR>')
+map('n', 'ss', ':split<Return><C-w>w', { silent = true })
+map('n', 'sv', ':vsplit<Return><C-w>w', { silent = true })
 
-keymap('n', 'x', '"_x', o) -- Do not yank with x
-keymap({ 'n', 'v', 'x' }, 'diw', '_diw', o) -- delete word without yanking
-keymap('n', 'cw', '"_ciw', o) -- change a word from anywhere without yanking
-keymap('n', 'cc', '"_cc', o) -- change line without yanking
-keymap('v', 'c', '"_c', o) -- change selection without yanking
-keymap('v', 'p', '"_dP', o) -- override selected word without yanking it
+map('n', '<Space>', '<C-w>w')
+map('', 's<left>', '<C-w>h')
+map('', 's<up>', '<C-w>k')
+map('', 's<down>', '<C-w>j')
+map('n', 'yw', 'yiw', o) -- yank a word from anywhere
+map('', 's<right>', '<C-w>l')
+map('', 'sh', '<C-w>h')
+map('', 'sk', '<C-w>k')
+map('', 'sj', '<C-w>j')
+map('', 'sl', '<C-w>l')
+
+map('n', '<C-w><left>', '<C-w><')
+map('n', '<C-w><right>', '<C-w>>')
+map('n', '<C-w><up>', '<C-w>+')
+map('n', '<C-w><down>', '<C-w>-')
+
+map('n', '<C-/>', ':s/^/#<CR>')
+
+map('n', 'x', '"_x', o) -- Do not yank with x
+map({ 'n', 'v', 'x' }, 'diw', '"_diw', o) -- delete word without yanking
+map('n', 'cw', '"_ciw', o) -- change a word from anywhere without yanking
+map('n', 'cc', '"_cc', o) -- change line without yanking
+map('v', 'c', '"_c', o) -- change selection without yanking
+map('v', 'p', '"_dP', o) -- override selected word without yanking it
