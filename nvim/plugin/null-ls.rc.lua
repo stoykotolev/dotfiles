64e7c34918dae0fe43a107bcc243fb8de1b814ae
Stoyko Tolev <stoyko@Stoykos-MacBook-Pro.local>
2022-12-19 21:49:22 +0200

    feat: null_ls config.

--- nvim/plugin/null-ls.rc.lua
+++ nvim/plugin/null-ls.rc.lua
@@ -7,14 +7,15 @@ local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup({
 	sources = {
+		formatting.stylua,
 		formatting.prettierd,
 		formatting.prismafmt,
 		formatting.rustfmt,
-		diagnostics.eslint_d.with({
-			diagnostics_format = '[eslint] #{m}\n(#{c})'
-		}),
+		diagnostics.eslint_d,
 		code_actions.eslint_d,
 	},
 	on_attach = function(client, bufnr)
@@ -33,10 +34,10 @@ null_ls.setup({
 	end
 })
 
-vim.api.nvim_create_user_command(
-	'DisableLspFormatting',
-	function()
-		vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 })
-	end,
-	{ nargs = 0 }
-)
+-- vim.api.nvim_create_user_command(
+-- 	'DisableLspFormatting',
+-- 	function()
+-- 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 })
+-- 	end,
+-- 	{ nargs = 0 }
+-- )
