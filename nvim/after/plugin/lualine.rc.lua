bf7c5df86dd0938ae3311e53a168b972a1723cb7
Stoyko Tolev <st.tolev@gmail.com>
2023-01-24 23:28:14 +0200

    feat: fix lualine. add better color for git.

--- nvim/after/plugin/lualine.rc.lua
+++ nvim/after/plugin/lualine.rc.lua
@@ -4,7 +4,7 @@ if (not present) then return end
 lualine.setup {
 	options = {
 		icons_enabled = true,
-		theme = 'catpuccin',
+		theme = 'catppuccin',
 		section_separators = { left = '', right = '' },
 		component_separators = { left = '', right = '' },
 		disabled_filetypes = {},
