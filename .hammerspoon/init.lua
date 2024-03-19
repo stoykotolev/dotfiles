461ca00bc142fc6809a31f51f3b2c7182cac6e65
Stoyko Tolev <st.tolev@gmail.com>
2024-03-19 19:17:56 +0200

    feat(hammerspoon): Update the hotkey to show/hide alacritty

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,8 +1,8 @@
-hs.hotkey.bind({ "cmd" }, "\\", function()
-  local alacritty = hs.application.find('alacritty')
-  if alacritty:isFrontmost() then
-    alacritty:hide()
-  else
-    hs.application.launchOrFocus("/Applications/Alacritty.app")
-  end
+hs.hotkey.bind({ "ctrl" }, ";", function()
+	local alacritty = hs.application.find("alacritty")
+	if alacritty:isFrontmost() then
+		alacritty:hide()
+	else
+		hs.application.launchOrFocus("/Applications/Alacritty.app")
+	end
 end)
