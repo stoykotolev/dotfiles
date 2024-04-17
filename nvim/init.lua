b87a6c8fd403a3445d4faf3f8e4369e23c8d31d7
Stoyko Tolev <st.tolev@gmail.com>
2024-04-17 10:24:22 +0300

    feat(gitsigns): Update gitsigns formatting.

--- nvim/init.lua
+++ nvim/init.lua
@@ -139,6 +139,17 @@ require('lazy').setup {
         untracked = { text = '│' },
       },
       current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+        delay = 1000,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+      },
+      current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+      current_line_blame_formatter_opts = {
+        relative_time = false,
+      },
     },
   },
   {
