872019066e3ec067314ef5a5a788a723378c35b4
Stoyko Tolev <st.tolev@gmail.com>
2024-05-14 20:52:31 +0300

    feat(nvim): Add new color scheme

--- nvim/init.lua
+++ nvim/init.lua
@@ -126,6 +126,23 @@ end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup {
+  -- colorscheme
+  -- {
+  --   'scottmckendry/cyberdream.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require('cyberdream').setup {
+  --       -- Recommended - see "Configuring" below for more config options
+  --       transparent = true,
+  --       italic_comments = true,
+  --       hide_fillchars = true,
+  --       borderless_telescope = true,
+  --       terminal_colors = true,
+  --     }
+  --     vim.cmd 'colorscheme cyberdream' -- set the colorscheme
+  --   end,
+  -- },
   {
     'neanias/everforest-nvim',
     version = false,
