788a257f038d88eabff580dc87dd2e52a0d5240a
Stoyko Tolev <st.tolev@gmail.com>
2024-05-14 20:56:09 +0300

    feat(nvim): Add oil as file search

--- nvim/init.lua
+++ nvim/init.lua
@@ -867,4 +867,26 @@ require('lazy').setup {
       vim.keymap.set('n', 'zk', require('ufo').peekFoldedLinesUnderCursor)
     end,
   },
+  {
+    'stevearc/oil.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('oil').setup {
+        columns = { 'icon' },
+        keymaps = {
+          ['<C-h>'] = false,
+          ['<M-h>'] = 'actions.select_split',
+        },
+        view_options = {
+          show_hidden = true,
+        },
+      }
+
+      -- Open parent directory in current window
+      vim.keymap.set('n', '<leader>t', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+
+      -- Open parent directory in floating window
+      vim.keymap.set('n', '<leader>ft', require('oil').toggle_float)
+    end,
+  },
 }
