3dbc968ef0328fa2fbf547d34a3a7e31946f9d1a
Stoyko Tolev <st.tolev@gmail.com>
2024-07-08 09:51:28 +0300

    feat(gitsigns): Remove deprecated current_line_blame_formatter_opts

--- nvim/init.lua
+++ nvim/init.lua
@@ -180,9 +180,6 @@ require('lazy').setup {
         virt_text_priority = 100,
       },
       current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
-      current_line_blame_formatter_opts = {
-        relative_time = false,
-      },
     },
   },
   {
