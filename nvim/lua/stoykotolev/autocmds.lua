b8b0aabe24373546599650aa3693179bdf2fe85a
Stoyko Tolev <st.tolev@gmail.com>
2024-08-31 01:55:04 +0300

    feat: setup autocmds

---
+++ nvim/lua/stoykotolev/autocmds.lua
@@ -0,0 +1,17 @@
+-- Disable the concealing in some file formats
+-- The default conceallevel is 3 in LazyVim
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "json", "jsonc", "markdown" },
+	callback = function()
+		vim.opt.conceallevel = 0
+	end,
+})
+
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
