99c86e7e6838c28588df9ab09171285624a9a68c
Stoyko Tolev <st.tolev@gmail.com>
2024-08-31 19:58:13 +0300

    feat: switch to luasnip due to bug

--- nvim/lua/stoykotolev/plugins/autocomplete.lua
+++ nvim/lua/stoykotolev/plugins/autocomplete.lua
@@ -1,80 +1,131 @@
 return {
-{
-    "hrsh7th/nvim-cmp",
-    -- load cmp on InsertEnter
-    event = "InsertEnter",
-    -- these dependencies will only be loaded when cmp loads
-    -- dependencies are always lazy-loaded unless specified otherwise
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
-      'onsails/lspkind.nvim',
- {
-    'rafamadriz/friendly-snippets',
-  },
-    },
-    config = function()
-      local cmp = require 'cmp'
-      local lspkind = require 'lspkind'
-
-      cmp.setup {
-        snippet = {
-          expand = function(args)
-	      vim.snippet.expand(args.body)
-          end,
-        },
-        completion = { completeopt = 'menu,menuone,noinsert' },
-        window = {
-          completion = {
-            col_offset = -3, -- align the abbr and word on cursor (due to fields order below)
-          },
-        },
-
-        formatting = {
-          fields = { 'kind', 'abbr', 'menu' },
-          format = lspkind.cmp_format {
-            mode = 'symbol_text', -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
-            maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-            menu = { -- showing type in menu
-              nvim_lsp = '[LSP]',
-              path = '[Path]',
-              buffer = '[Buffer]',
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        dependencies = {
+            -- Snippet Engine & its associated nvim-cmp source
+            {
+                "L3MON4D3/LuaSnip",
+                build = (function()
+                    if
+                        vim.fn.has("win32") == 1
+                        or vim.fn.executable("make") == 0
+                    then
+                        return
+                    end
+                    return "make install_jsregexp"
+                end)(),
+                dependencies = {
+                    {
+                        "rafamadriz/friendly-snippets",
+                        config = function()
+                            require("luasnip.loaders.from_vscode").lazy_load()
+                        end,
+                    },
+                },
             },
-            before = function(entry, vim_item) -- for tailwind css autocomplete
-              if vim_item.kind == 'Color' and entry.completion_item.documentation then
-                local _, _, r, g, b = string.find(entry.completion_item.documentation, '^rgb%((%d+), (%d+), (%d+)')
-                if r then
-                  local color = string.format('%02x', r) .. string.format('%02x', g) .. string.format('%02x', b)
-                  local group = 'Tw_' .. color
-                  if vim.fn.hlID(group) < 1 then
-                    vim.api.nvim_set_hl(0, group, { fg = '#' .. color })
-                  end
-                  vim_item.kind = '■' -- or "⬤" or anything
-                  vim_item.kind_hl_group = group
-                  return vim_item
-                end
-              end
-              -- vim_item.kind = icons[vim_item.kind] and (icons[vim_item.kind] .. vim_item.kind) or vim_item.kind
-              -- or just show the icon
-              vim_item.kind = lspkind.symbolic(vim_item.kind) and lspkind.symbolic(vim_item.kind) or vim_item.kind
-              return vim_item
-            end,
-          },
+            "saadparwaiz1/cmp_luasnip",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-path",
+            "onsails/lspkind.nvim",
         },
+        config = function()
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
+            local lspkind = require("lspkind")
+            luasnip.config.setup({})
 
-        mapping = cmp.mapping.preset.insert {
-          ['<C-j>'] = cmp.mapping.select_next_item(), -- Select the [n]ext item
-          ['<C-k>'] = cmp.mapping.select_prev_item(), -- Select the [p]revious item
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4), -- Scroll the documentation window [b]ack
-          ['<C-f>'] = cmp.mapping.scroll_docs(4), -- and [f]orward
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
-          ['<C-o>'] = cmp.mapping.complete {},
-        },
-        sources = {
-          { name = 'nvim_lsp' },
-          { name = 'path' },
-        },
-      }
-    end,
-  },
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                completion = { completeopt = "menu,menuone,noinsert" },
+                window = {
+                    completion = {
+                        col_offset = -3, -- align the abbr and word on cursor (due to fields order below)
+                    },
+                },
+
+                formatting = {
+                    fields = { "kind", "abbr", "menu" },
+                    format = lspkind.cmp_format({
+                        mode = "symbol_text", -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+                        maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                        menu = { -- showing type in menu
+                            nvim_lsp = "[LSP]",
+                            path = "[Path]",
+                            buffer = "[Buffer]",
+                            luasnip = "[LuaSnip]",
+                        },
+                        before = function(entry, vim_item) -- for tailwind css autocomplete
+                            if
+                                vim_item.kind == "Color"
+                                and entry.completion_item.documentation
+                            then
+                                local _, _, r, g, b = string.find(
+                                    entry.completion_item.documentation,
+                                    "^rgb%((%d+), (%d+), (%d+)"
+                                )
+                                if r then
+                                    local color = string.format("%02x", r)
+                                        .. string.format("%02x", g)
+                                        .. string.format("%02x", b)
+                                    local group = "Tw_" .. color
+                                    if vim.fn.hlID(group) < 1 then
+                                        vim.api.nvim_set_hl(
+                                            0,
+                                            group,
+                                            { fg = "#" .. color }
+                                        )
+                                    end
+                                    vim_item.kind = "■" -- or "⬤" or anything
+                                    vim_item.kind_hl_group = group
+                                    return vim_item
+                                end
+                            end
+                            -- vim_item.kind = icons[vim_item.kind] and (icons[vim_item.kind] .. vim_item.kind) or vim_item.kind
+                            -- or just show the icon
+                            vim_item.kind = lspkind.symbolic(vim_item.kind)
+                                    and lspkind.symbolic(vim_item.kind)
+                                or vim_item.kind
+                            return vim_item
+                        end,
+                    }),
+                },
+
+                mapping = cmp.mapping.preset.insert({
+                    -- Select the [n]ext item
+                    ["<C-j>"] = cmp.mapping.select_next_item(),
+                    -- Select the [p]revious item
+                    ["<C-k>"] = cmp.mapping.select_prev_item(),
+
+                    -- Scroll the documentation window [b]ack / [f]orward
+                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+
+                    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+
+                    ["<C-o>"] = cmp.mapping.complete({}),
+
+                    ["<C-l>"] = cmp.mapping(function()
+                        if luasnip.expand_or_locally_jumpable() then
+                            luasnip.expand_or_jump()
+                        end
+                    end, { "i" }),
+                    ["<C-h>"] = cmp.mapping(function()
+                        if luasnip.locally_jumpable(-1) then
+                            luasnip.jump(-1)
+                        end
+                    end, { "i" }),
+                }),
+                sources = {
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                    { name = "path" },
+                },
+            })
+        end,
+    },
 }
