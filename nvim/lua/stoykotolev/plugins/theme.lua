2d27d3a0c7553a51c412627d269f1348703a6bf2
Stoyko Tolev <st.tolev@gmail.com>
2024-08-31 17:58:09 +0300

    feat: kanagawa theme setup

--- nvim/lua/stoykotolev/plugins/theme.lua
+++ nvim/lua/stoykotolev/plugins/theme.lua
@@ -2,7 +2,18 @@ return {
     {
         "rebelot/kanagawa.nvim",
         config = function()
-            vim.cmd([[colorscheme kanagawa]])
+            require("kanagawa").setup({
+                undercurl = true, -- enable undercurls
+                commentStyle = { italic = true },
+                functionStyle = {},
+                keywordStyle = { italic = true },
+                statementStyle = { bold = true },
+                typeStyle = {},
+                transparent = false, -- do not set background color
+                dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+                terminalColors = true, -- define vim.g.terminal_color_{0,17}
+            })
+            vim.cmd([[colorscheme kanagawa-dragon]])
         end,
     },
 }
