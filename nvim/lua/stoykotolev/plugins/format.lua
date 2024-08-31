b99df8a29de6d79470bb6cd08cad4ba6f75b839f
Stoyko Tolev <st.tolev@gmail.com>
2024-08-31 16:06:54 +0300

    feat: autopair, git, harpoon, todo

--- nvim/lua/stoykotolev/plugins/format.lua
+++ nvim/lua/stoykotolev/plugins/format.lua
@@ -1,31 +1,34 @@
 local autocmd = vim.api.nvim_create_autocmd
 return {
-{
-    'stevearc/conform.nvim',
-    lazy = false,
-    opts = {
-      notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 100,
-        lsp_fallback = true,
-      },
-      formatters_by_ft = {
-        lua = { 'stylua' },
-      },
+    {
+        "stevearc/conform.nvim",
+        lazy = false,
+        opts = {
+            notify_on_error = false,
+            format_on_save = {
+                timeout_ms = 100,
+                lsp_fallback = true,
+            },
+            formatters_by_ft = {
+                lua = { "stylua" },
+            },
+        },
     },
-  },
-  -- Linting
-  {
-    'mfussenegger/nvim-lint',
-    config = function()
-      local lint = require 'lint'
-      lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
-      }
-      autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-        callback = function()
-          require('lint').try_lint()
+    -- Linting
+    {
+        "mfussenegger/nvim-lint",
+        config = function()
+            local lint = require("lint")
+            lint.linters_by_ft = {
+                markdown = { "markdownlint" },
+            }
+            autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+                callback = function()
+                    require("lint").try_lint()
+                end,
+            })
         end,
-      })
-    end,
-  },}
+    },
+    "tpope/vim-sleuth",
+    { "numToStr/Comment.nvim", opts = {} },
+}
---
+++ nvim/lua/stoykotolev/plugins/git.lua
@@ -0,0 +1,24 @@
+return {
+    {
+        "lewis6991/gitsigns.nvim",
+        opts = {
+            signs = {
+                add = { text = "+" },
+                change = { text = "~" },
+                delete = { text = "_" },
+                topdelete = { text = "‾" },
+                changedelete = { text = "~" },
+                untracked = { text = "│" },
+            },
+            current_line_blame = true,
+            current_line_blame_opts = {
+                virt_text = true,
+                virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+                delay = 1000,
+                ignore_whitespace = false,
+                virt_text_priority = 100,
+            },
+            current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+        },
+    },
+}
