0b306a5b15994a74d6fbd43a5cc163d3bd38f0cb
Stoyko Tolev <st.tolev@gmail.com>
2024-08-31 13:28:21 +0300

    feat: remove luasnip and use builting vim snippet

--- nvim/lua/stoykotolev/plugins/autocomplete.lua
+++ nvim/lua/stoykotolev/plugins/autocomplete.lua
@@ -6,38 +6,21 @@ return {
     -- these dependencies will only be loaded when cmp loads
     -- dependencies are always lazy-loaded unless specified otherwise
     dependencies = {
-      {
-        'L3MON4D3/LuaSnip',
-        build = (function()
-          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
-        dependencies = {
-          {
-            'rafamadriz/friendly-snippets',
-            config = function()
-              require('luasnip.loaders.from_vscode').lazy_load()
-            end,
-          },
-        },
-      },
-      'saadparwaiz1/cmp_luasnip',
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
       'onsails/lspkind.nvim',
+ {
+    'rafamadriz/friendly-snippets',
+  },
     },
     config = function()
       local cmp = require 'cmp'
-      local luasnip = require 'luasnip'
       local lspkind = require 'lspkind'
-      luasnip.config.setup {}
 
       cmp.setup {
         snippet = {
           expand = function(args)
-            luasnip.lsp_expand(args.body)
+	      vim.snippet.expand(args.body)
           end,
         },
         completion = { completeopt = 'menu,menuone,noinsert' },
@@ -56,7 +39,6 @@ return {
               nvim_lsp = '[LSP]',
               path = '[Path]',
               buffer = '[Buffer]',
-              luasnip = '[LuaSnip]',
             },
             before = function(entry, vim_item) -- for tailwind css autocomplete
               if vim_item.kind == 'Color' and entry.completion_item.documentation then
@@ -81,33 +63,15 @@ return {
         },
 
         mapping = cmp.mapping.preset.insert {
-          -- Select the [n]ext item
-          ['<C-j>'] = cmp.mapping.select_next_item(),
-          -- Select the [p]revious item
-          ['<C-k>'] = cmp.mapping.select_prev_item(),
-
-          -- Scroll the documentation window [b]ack / [f]orward
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-
+          ['<C-j>'] = cmp.mapping.select_next_item(), -- Select the [n]ext item
+          ['<C-k>'] = cmp.mapping.select_prev_item(), -- Select the [p]revious item
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4), -- Scroll the documentation window [b]ack
+          ['<C-f>'] = cmp.mapping.scroll_docs(4), -- and [f]orward
           ['<C-y>'] = cmp.mapping.confirm { select = true },
-
           ['<C-o>'] = cmp.mapping.complete {},
-
-          ['<C-l>'] = cmp.mapping(function()
-            if luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            end
-          end, { 'i' }),
-          ['<C-h>'] = cmp.mapping(function()
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            end
-          end, { 'i' }),
         },
         sources = {
           { name = 'nvim_lsp' },
-          { name = 'luasnip' },
           { name = 'path' },
         },
       }
