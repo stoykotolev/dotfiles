821a01baf969052e10c5fbce3a00a036ac8ca133
Stoyko Tolev <st.tolev@gmail.com>
2024-08-31 14:39:48 +0300

    feat: setup formatting and linting on save

---
+++ nvim/lua/stoykotolev/plugins/format.lua
@@ -0,0 +1,31 @@
+local autocmd = vim.api.nvim_create_autocmd
+return {
+{
+    'stevearc/conform.nvim',
+    lazy = false,
+    opts = {
+      notify_on_error = false,
+      format_on_save = {
+        timeout_ms = 100,
+        lsp_fallback = true,
+      },
+      formatters_by_ft = {
+        lua = { 'stylua' },
+      },
+    },
+  },
+  -- Linting
+  {
+    'mfussenegger/nvim-lint',
+    config = function()
+      local lint = require 'lint'
+      lint.linters_by_ft = {
+        markdown = { 'markdownlint' },
+      }
+      autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+        callback = function()
+          require('lint').try_lint()
+        end,
+      })
+    end,
+  },}
--- nvim/lua/stoykotolev/plugins/lsp.lua
+++ nvim/lua/stoykotolev/plugins/lsp.lua
@@ -1,110 +1,163 @@
 local autocmd = vim.api.nvim_create_autocmd
 return {
-	{
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "WhoIsSethDaniel/mason-tool-installer.nvim",
 
-      { 'j-hui/fidget.nvim', opts = {} },
+            { "j-hui/fidget.nvim", opts = {} },
 
-      { 'folke/neodev.nvim', opts = {} },
+            { "folke/neodev.nvim", opts = {} },
+        },
+        config = function()
+            vim.api.nvim_create_autocmd("LspAttach", {
+                group = vim.api.nvim_create_augroup(
+                    "kickstart-lsp-attach",
+                    { clear = true }
+                ),
+                callback = function(event)
+                    local lsp_map = function(keys, func, desc)
+                        vim.keymap.set(
+                            "n",
+                            keys,
+                            func,
+                            { buffer = event.buf, desc = "LSP: " .. desc }
+                        )
+                    end
+                    lsp_map("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+                    lsp_map("gr", vim.lsp.buf.references, "[G]oto [R]eferences")
+                    lsp_map(
+                        "gI",
+                        vim.lsp.buf.implementation,
+                        "[G]oto [I]mplementation"
+                    )
+                    lsp_map(
+                        "<leader>D",
+                        vim.lsp.buf.type_definition,
+                        "Type [D]efinition"
+                    )
+                    lsp_map(
+                        "<leader>ds",
+                        vim.lsp.buf.document_symbol,
+                        "[D]ocument [S]ymbols"
+                    )
+                    lsp_map(
+                        "<leader>ws",
+                        vim.lsp.buf.workspace_symbol,
+                        "[W]orkspace [S]ymbols"
+                    )
+                    lsp_map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+                    lsp_map(
+                        "<leader>ca",
+                        vim.lsp.buf.code_action,
+                        "[C]ode [A]ction"
+                    )
+                    lsp_map("K", vim.lsp.buf.hover, "Hover Documentation")
+                    lsp_map(
+                        "gD",
+                        vim.lsp.buf.declaration,
+                        "[G]oto [D]eclaration"
+                    )
+
+                    -- The following two autocommands are used to highlight references of the
+                    -- word under your cursor when your cursor rests there for a little while.
+                    -- When you move your cursor, the highlights will be cleared (the second autocommand).
+                    local client =
+                        vim.lsp.get_client_by_id(event.data.client_id)
+                    if
+                        client
+                        and client.server_capabilities.documentHighlightProvider
+                    then
+                        autocmd({ "CursorHold", "CursorHoldI" }, {
+                            buffer = event.buf,
+                            callback = vim.lsp.buf.document_highlight,
+                        })
+
+                        autocmd({ "CursorMoved", "CursorMovedI" }, {
+                            buffer = event.buf,
+                            callback = vim.lsp.buf.clear_references,
+                        })
+                    end
+                end,
+            })
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = vim.tbl_deep_extend(
+                "force",
+                capabilities,
+                require("cmp_nvim_lsp").default_capabilities()
+            )
+            local servers = {
+                lua_ls = {
+                    settings = {
+                        Lua = {
+                            completion = {
+                                callSnippet = "Replace",
+                            },
+                            diagnostics = { disable = { "missing-fields" } },
+                        },
+                    },
+                },
+                gopls = {
+                    capabilities = capabilities,
+                    cmd = { "gopls" },
+                    filetypes = { "go", "gomod", "gowork", "gotmpl" },
+                    root_dir = require("lspconfig/util").root_pattern(
+                        "go.work",
+                        "go.mod"
+                    ),
+                    settings = {
+                        gopls = {
+                            completeUnimported = true,
+                            usePlaceholders = true,
+                            analyses = {
+                                unusedparams = true,
+                            },
+                        },
+                    },
+                },
+                marksman = {},
+            }
+
+            require("mason").setup()
+
+            local ensure_installed = vim.tbl_keys(servers or {})
+            vim.list_extend(ensure_installed, {
+                "markdownlint",
+                "stylua",
+                "gofumpt",
+                "golines",
+                "goimports-reviser",
+                "lua-language-server",
+            })
+
+            require("mason-tool-installer").setup({
+                ensure_installed = ensure_installed,
+            })
+
+            require("mason-lspconfig").setup({
+                handlers = {
+                    function(server_name)
+                        local server = servers[server_name] or {}
+                        -- This handles overriding only values explicitly passed
+                        -- by the server configuration above. Useful when disabling
+                        -- certain features of an LSP (for example, turning off formatting for tsserver)
+                        server.capabilities = vim.tbl_deep_extend(
+                            "force",
+                            {},
+                            capabilities,
+                            server.capabilities or {}
+                        )
+                        require("lspconfig")[server_name].setup(server)
+                    end,
+                },
+            })
+        end,
     },
-    config = function()
-	vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-	callback = function(event)
-          local lsp_map = function(keys, func, desc)
-            vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-          end
-          lsp_map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-          lsp_map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-          lsp_map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-          lsp_map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-          lsp_map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-          lsp_map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-          lsp_map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-          lsp_map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-          lsp_map('K', vim.lsp.buf.hover, 'Hover Documentation')
-          lsp_map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-	  -- The following two autocommands are used to highlight references of the
-          -- word under your cursor when your cursor rests there for a little while.
-          -- When you move your cursor, the highlights will be cleared (the second autocommand).
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
-          if client and client.server_capabilities.documentHighlightProvider then
-            autocmd({ 'CursorHold', 'CursorHoldI' }, {
-              buffer = event.buf,
-              callback = vim.lsp.buf.document_highlight,
-            })
-
-            autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-              buffer = event.buf,
-              callback = vim.lsp.buf.clear_references,
-            })
-          end
-	end
-      }
-	)
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-      local servers = {
-        lua_ls = {
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-              diagnostics = { disable = { 'missing-fields' } },
-            },
-          },
-        },
-        gopls = {
-          capabilities = capabilities,
-          cmd = { 'gopls' },
-          filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
-          root_dir = require('lspconfig/util').root_pattern('go.work', 'go.mod'),
-          settings = {
-            gopls = {
-              completeUnimported = true,
-              usePlaceholders = true,
-              analyses = {
-                unusedparams = true,
-              },
-            },
-          },
-        },
-        marksman = {},
-      }
-
-      require('mason').setup()
-
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'markdownlint',
-        'stylua',
-        'gofumpt',
-        'golines',
-        'goimports-reviser',
-        'lua-language-server',
-      })
-
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-      require('mason-lspconfig').setup {
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for tsserver)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
-      }
-    end
-    }
+    {
+        "mrcjkb/rustaceanvim",
+        version = "^4", -- Recommended
+        ft = { "rust" },
+    },
 }
-
