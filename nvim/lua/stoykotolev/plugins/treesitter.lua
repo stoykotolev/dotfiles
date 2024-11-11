42d19ae0e530e20491384f958f4958c8aa2adb8b
Stoyko Tolev <st.tolev@gmail.com>
2024-11-11 09:36:05 +0000

    feat: Configure treesitter context

--- nvim/lua/stoykotolev/plugins/treesitter.lua
+++ nvim/lua/stoykotolev/plugins/treesitter.lua
@@ -35,7 +35,9 @@ return {
     {
         "nvim-treesitter/nvim-treesitter-context",
         config = function()
-            require("treesitter-context").setup()
+            require("treesitter-context").setup({
+                max_lines = 1
+            })
         end,
     },
 }
