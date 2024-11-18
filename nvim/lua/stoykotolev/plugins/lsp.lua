a320855ad801926f61237b5b3eac4a4a1fe0c056
Stoyko Tolev <st.tolev@gmail.com>
2024-11-18 23:39:06 +0200

    feat: Setup typescript tools instead of ts server. Setup lint

--- nvim/lua/stoykotolev/plugins/lsp.lua
+++ nvim/lua/stoykotolev/plugins/lsp.lua
@@ -171,22 +171,6 @@ return {
                         ".git"
                     ),
                 },
-                ["typescript-language-server"] = {
-                    cmd = { "typescript-language-server", "--stdio" },
-                    capabilties = capabilities,
-                    filetypes = {
-                        "javascript",
-                        "javascriptreact",
-                        "typescript",
-                        "typescriptreact",
-                        "html",
-                    },
-                    root_dir = require("lspconfig").util.root_pattern(
-                        "package.json",
-                        "tsconfig.json",
-                        ".git"
-                    ),
-                },
                 marksman = {},
             }
 
@@ -237,4 +221,31 @@ return {
         version = "^4", -- Recommended
         ft = { "rust" },
     },
+    {
+        'pmizio/typescript-tools.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+        config = function()
+            local api = require 'typescript-tools.api'
+            require('typescript-tools').setup {
+                handlers = {
+                    ['textDocument/publishDiagnostics'] = api.filter_diagnostics { 6133 },
+                },
+                settings = {
+                    tsserver_file_preferences = {
+                        importModuleSpecifierPreference = 'non-relative',
+                    },
+                },
+            }
+            local autocmd = vim.api.nvim_create_autocmd
+            autocmd('BufWritePre', {
+                pattern = '*.ts,*.tsx,*.jsx,*.js',
+                callback = function(args)
+                    vim.cmd 'TSToolsAddMissingImports sync'
+                    vim.cmd 'TSToolsOrganizeImports sync'
+                    require('conform').format { bufnr = args.buf }
+                end,
+            })
+        end,
+
+    }
 }
