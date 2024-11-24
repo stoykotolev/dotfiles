46a1eee3d0d95396c669f0375f811a2b62779f55
Stoyko Tolev <st.tolev@gmail.com>
2024-11-24 13:18:07 +0200

    feat: setup noice

--- nvim/lua/stoykotolev/plugins/lsp.lua
+++ nvim/lua/stoykotolev/plugins/lsp.lua
@@ -7,7 +7,16 @@ return {
             "williamboman/mason-lspconfig.nvim",
             "WhoIsSethDaniel/mason-tool-installer.nvim",
 
-            { "j-hui/fidget.nvim", opts = {} },
+            {
+                "j-hui/fidget.nvim",
+                opts = {
+                    notification = {
+                        window = {
+                            winblend = 0
+                        }
+                    }
+                }
+            },
 
             { "folke/neodev.nvim", opts = {} },
         },
--- nvim/lua/stoykotolev/plugins/noice.lua
+++ nvim/lua/stoykotolev/plugins/noice.lua
@@ -1,12 +1,46 @@
 return {
-    "folke/noice.nvim",
-    event = "VeryLazy",
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+        lsp = {
+            -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+            override = {
+                ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+                ['vim.lsp.util.stylize_markdown'] = true,
+                ['cmp.entry.get_documentation'] = true,
+            },
+            progress = {
+                enabled = false
+            }
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+            bottom_search = true,         -- use a classic bottom cmdline for search
+            long_message_to_split = true, -- long messages will be sent to a split
+            inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+            lsp_doc_border = false,       -- add a border to hover docs and signature help
+        },
+        messages = {
+            enabled = true,              -- enables the Noice messages UI
+            view = 'notify',             -- default view for messages
+            view_error = 'notify',       -- view for errors
+            view_warn = 'notify',        -- view for warnings
+            view_history = 'messages',   -- view for :messages
+            view_search = 'virtualtext', -- view for search count messages. Set to `false` to disable
+        },
+    },
     dependencies = {
         -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-        "MunifTanjim/nui.nvim",
+        'MunifTanjim/nui.nvim',
         -- OPTIONAL:
         --   `nvim-notify` is only needed, if you want to use the notification view.
         --   If not available, we use `mini` as the fallback
-        "rcarriga/nvim-notify",
+        'rcarriga/nvim-notify',
     },
+    config = function()
+        require("noice").setup(opts)
+        require("notify").setup({
+            background_colour = "#36454f"
+        })
+    end
 }
