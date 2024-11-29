b77ba0fb618e1cd713816c5ed5d2cc22ff489cbc
Stoyko Tolev <st.tolev@gmail.com>
2024-11-29 11:32:47 +0200

    feat: remove unnecessary autocmd

--- nvim/lua/stoykotolev/plugins/conform.lua
+++ nvim/lua/stoykotolev/plugins/conform.lua
@@ -17,12 +17,5 @@ return {
                 lsp_format = "fallback",
             },
         })
-
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            pattern = "*",
-            callback = function(args)
-                require("conform").format({ bufnr = args.buf })
-            end,
-        })
     end,
 }
