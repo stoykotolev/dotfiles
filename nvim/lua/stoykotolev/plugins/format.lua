288f9eb701857bef46bd16ba4bec957614787dc4
Stoyko Tolev <st.tolev@gmail.com>
2024-12-18 23:41:23 +0200

    feat: add missing formatters

--- nvim/lua/stoykotolev/plugins/format.lua
+++ nvim/lua/stoykotolev/plugins/format.lua
@@ -11,6 +11,9 @@ return {
             },
             formatters_by_ft = {
                 lua = { "stylua" },
+                javascript = { "prettierd" },
+                typescript = { "prettierd" },
+                typescriptreact = { "prettierd" }
             },
         },
     },
