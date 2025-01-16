6c85ce5c4bc1cd27011e0332906cd4fd79582b81
Stoyko Tolev <st.tolev@gmail.com>
2025-01-16 13:05:23 +0200

    fix: lua syntax

--- nvim/lua/stoykotolev/plugins/harpoon.lua
+++ nvim/lua/stoykotolev/plugins/harpoon.lua
@@ -21,24 +21,24 @@ return {
 
             vim.keymap.set("n", "<leader>h", function()
                 harpoon:list():select(1)
-            end,{
-            desc: "Go to 1st harpoon file",
-        })
+            end, {
+                desc = "Go to 1st harpoon file",
+            })
             vim.keymap.set("n", "<leader>j", function()
                 harpoon:list():select(2)
-            end,{
-            desc: "Go to 2nd harpoon file",
-        })
+            end, {
+                desc = "Go to 2nd harpoon file",
+            })
             vim.keymap.set("n", "<leader>k", function()
                 harpoon:list():select(3)
-            end,{
-            desc: "Go to 3rd harpoon file",
-        })
+            end, {
+                desc = "Go to 3rd harpoon file",
+            })
             vim.keymap.set("n", "<leader>l", function()
                 harpoon:list():select(4)
             end, {
-            desc: "Go to 4th harpoon file",
-        })
+                desc = "Go to 4th harpoon file",
+            })
         end,
     },
 }
