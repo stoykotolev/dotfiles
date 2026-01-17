1c26b4b59aef886e1518fbcc4e303e5caebeace3
Stoyko Tolev <st.tolev@gmail.com>
2026-01-17 17:16:13 +0200

    feat: Setup mini file for some mini libs

--- nvim/plugin/30_mini.lua
+++ nvim/plugin/30_mini.lua
@@ -1,2 +1,125 @@
 local now, later = MiniDeps.now, MiniDeps.later
 local now_if_args = _G.Config.now_if_args
+
+
+-- Icons
+now(function()
+  -- Set up to not prefer extension-based icon for some extensions
+  local ext3_blocklist = { scm = true, txt = true, yml = true }
+  local ext4_blocklist = { json = true, yaml = true }
+  require('mini.icons').setup({
+    use_file_extension = function(ext, _)
+      return not (ext3_blocklist[ext:sub(-3)] or ext4_blocklist[ext:sub(-4)])
+    end,
+  })
+
+  -- Mock 'nvim-tree/nvim-web-devicons' for plugins without 'mini.icons' support.
+  -- Not needed for 'mini.nvim' or MiniMax, but might be useful for others.
+  later(MiniIcons.mock_nvim_web_devicons)
+
+  -- Add LSP kind icons. Useful for 'mini.completion'.
+  later(MiniIcons.tweak_lsp_kind)
+end)
+
+later(function() require('mini.surround').setup() end)
+later(function() require('mini.extra').setup() end)
+now(function() require('mini.notify').setup() end)
+
+now(function() require('mini.statusline').setup() end)
+-- Snippets
+later(function()
+  -- Define language patterns to work better with 'friendly-snippets'
+  local lang_patterns = {
+    markdown_inline = { 'markdown.json' },
+  }
+
+  local snippets = require('mini.snippets')
+  local config_path = vim.fn.stdpath('config')
+  snippets.setup({
+    snippets = {
+      snippets.gen_loader.from_file(config_path .. '/snippets/global.json'),
+      snippets.gen_loader.from_lang({ lang_patterns = lang_patterns }),
+    },
+  })
+  MiniSnippets.start_lsp_server()
+end)
+
+later(function() require('mini.pick').setup() end)
+
+later(function()
+  -- Create pairs not only in Insert, but also in Command line mode
+  require('mini.pairs').setup({ modes = { command = true } })
+end)
+
+later(function()
+  -- local hipatterns = require('mini.hipatterns')
+  -- local hi_words = MiniExtra.gen_highlighter.words
+  -- hipatterns.setup({
+  --   highlighters = {
+  --     -- Highlight a fixed set of common words. Will be highlighted in any place,
+  --     -- not like "only in comments".
+  --     fixme = hi_words({ 'FIXME', 'Fixme', 'fixme' }, 'MiniHipatternsFixme'),
+  --     hack = hi_words({ 'HACK', 'Hack', 'hack' }, 'MiniHipatternsHack'),
+  --     todo = hi_words({ 'TODO', 'Todo', 'todo' }, 'MiniHipatternsTodo'),
+  --     note = hi_words({ 'NOTE', 'Note', 'note' }, 'MiniHipatternsNote'),
+  --
+  --     -- Highlight hex color string (#aabbcc) with that color as a background
+  --     hex_color = hipatterns.gen_highlighter.hex_color(),
+  --   },
+  -- })
+end)
+
+later(function()
+  -- Customize post-processing of LSP responses for a better user experience.
+  -- Don't show 'Text' suggestions (usually noisy) and show snippets last.
+  local process_items_opts = { kind_priority = { Text = -1, Snippet = 99 } }
+  local process_items = function(items, base)
+    return MiniCompletion.default_process_items(items, base, process_items_opts)
+  end
+  require('mini.completion').setup({
+    lsp_completion = {
+      -- Without this config autocompletion is set up through `:h 'completefunc'`.
+      -- Although not needed, setting up through `:h 'omnifunc'` is cleaner
+      -- (sets up only when needed) and makes it possible to use `<C-u>`.
+      source_func = 'omnifunc',
+      auto_setup = false,
+      process_items = process_items,
+    },
+  })
+
+  -- Set 'omnifunc' for LSP completion only when needed.
+  local on_attach = function(ev)
+    vim.bo[ev.buf].omnifunc = 'v:lua.MiniCompletion.completefunc_lsp'
+  end
+  _G.Config.new_autocmd('LspAttach', nil, on_attach, "Set 'omnifunc'")
+
+  -- Advertise to servers that Neovim now supports certain set of completion and
+  -- signature features through 'mini.completion'.
+  vim.lsp.config('*', { capabilities = MiniCompletion.get_lsp_capabilities() })
+end)
+
+later(function() require('mini.cmdline').setup() end)
+
+-- Text objects
+later(function()
+  local ai = require('mini.ai')
+  ai.setup({
+    -- 'mini.ai' can be extended with custom textobjects
+    custom_textobjects = {
+      -- Make `aB` / `iB` act on around/inside whole *b*uffer
+      B = MiniExtra.gen_ai_spec.buffer(),
+      -- For more complicated textobjects that require structural awareness,
+      -- use tree-sitter. This example makes `aF`/`iF` mean around/inside function
+      -- definition (not call). See `:h MiniAi.gen_spec.treesitter()` for details.
+      F = ai.gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }),
+    },
+
+    -- 'mini.ai' by default mostly mimics built-in search behavior: first try
+    -- to find textobject covering cursor, then try to find to the right.
+    -- Although this works in most cases, some are confusing. It is more robust to
+    -- always try to search only covering textobject and explicitly ask to search
+    -- for next (`an`/`in`) or last (`al`/`il`).
+    -- Try this. If you don't like it - delete next line and this comment.
+    search_method = 'cover',
+  })
+end)
