fb32dd9f2fb833a8de5cb863ff39264574566aa9
Stoyko Tolev <st.tolev@gmail.com>
2026-01-17 18:49:55 +0200

    feat: Add stylua. Add colorizer.

--- nvim/plugin/30_mini.lua
+++ nvim/plugin/30_mini.lua
@@ -1,7 +1,6 @@
 local now, later = MiniDeps.now, MiniDeps.later
 local now_if_args = _G.Config.now_if_args
 
-
 -- Icons
 now(function()
   -- Set up to not prefer extension-based icon for some extensions
@@ -52,21 +51,21 @@ later(function()
 end)
 
 later(function()
-  -- local hipatterns = require('mini.hipatterns')
-  -- local hi_words = MiniExtra.gen_highlighter.words
-  -- hipatterns.setup({
-  --   highlighters = {
-  --     -- Highlight a fixed set of common words. Will be highlighted in any place,
-  --     -- not like "only in comments".
-  --     fixme = hi_words({ 'FIXME', 'Fixme', 'fixme' }, 'MiniHipatternsFixme'),
-  --     hack = hi_words({ 'HACK', 'Hack', 'hack' }, 'MiniHipatternsHack'),
-  --     todo = hi_words({ 'TODO', 'Todo', 'todo' }, 'MiniHipatternsTodo'),
-  --     note = hi_words({ 'NOTE', 'Note', 'note' }, 'MiniHipatternsNote'),
-  --
-  --     -- Highlight hex color string (#aabbcc) with that color as a background
-  --     hex_color = hipatterns.gen_highlighter.hex_color(),
-  --   },
-  -- })
+  local hipatterns = require('mini.hipatterns')
+  local hi_words = MiniExtra.gen_highlighter.words
+  hipatterns.setup({
+    highlighters = {
+      -- Highlight a fixed set of common words. Will be highlighted in any place,
+      -- not like "only in comments".
+      fixme = hi_words({ 'FIXME', 'Fixme', 'fixme' }, 'MiniHipatternsFixme'),
+      hack = hi_words({ 'HACK', 'Hack', 'hack' }, 'MiniHipatternsHack'),
+      todo = hi_words({ 'TODO', 'Todo', 'todo' }, 'MiniHipatternsTodo'),
+      note = hi_words({ 'NOTE', 'Note', 'note' }, 'MiniHipatternsNote'),
+
+      -- Highlight hex color string (#aabbcc) with that color as a background
+      hex_color = hipatterns.gen_highlighter.hex_color(),
+    },
+  })
 end)
 
 later(function()
