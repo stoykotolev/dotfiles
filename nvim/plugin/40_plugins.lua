8a650db6ecedd0113014cc0fcc5380cbb4e2e661
Stoyko Tolev <st.tolev@gmail.com>
2026-02-12 09:49:31 +0200

    feat: Remove noice. Enable tailwind lsp

--- nvim/plugin/40_plugins.lua
+++ nvim/plugin/40_plugins.lua
@@ -179,7 +179,7 @@ now_if_args(function()
   add('neovim/nvim-lspconfig')
   vim.lsp.enable({
     'lua_ls',
-    'tailwindcss-language-server',
+    'tailwindcss',
     'emmet_language_server',
     'html',
     'cssls',
@@ -215,44 +215,6 @@ later(function()
     current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
   })
 end)
-
--- later(function()
---   add({
---     source = 'folke/noice.nvim',
---     depends = { 'MunifTanjim/nui.nvim' },
---   })
---   require('noice').setup({
---     lsp = {
---       -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
---       override = {
---         ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
---         ['vim.lsp.util.stylize_markdown'] = true,
---         ['cmp.entry.get_documentation'] = true,
---       },
---       progress = {
---         enabled = false,
---       },
---       signature = {
---         enabled = false,
---       },
---     },
---     -- you can enable a preset for easier configuration
---     presets = {
---       bottom_search = true, -- use a classic bottom cmdline for search
---       long_message_to_split = true, -- long messages will be sent to a split
---       inc_rename = false, -- enables an input dialog for inc-rename.nvim
---       lsp_doc_border = false, -- add a border to hover docs and signature help
---     },
---     messages = {
---       enabled = false, -- enables the Noice messages UI
---       view = 'notify', -- default view for messages
---       view_error = 'notify', -- view for errors
---       view_warn = 'notify', -- view for warnings
---       view_history = 'messages', -- view for :messages
---       view_search = 'virtualtext', -- view for search count messages. Set to `false` to disable
---     },
---   })
--- end)
 
 later(function()
   add({
