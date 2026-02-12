e43dfe138d7fcabe26e96611f4618be869b70898
Stoyko Tolev <st.tolev@gmail.com>
2026-02-12 09:34:41 +0200

    feat: Add autocommand to close quick fix

--- nvim/plugin/50_autocmds.lua
+++ nvim/plugin/50_autocmds.lua
@@ -8,3 +8,11 @@ _G.Config.new_autocmd(
 -- Do on `FileType` to always override these changes from filetype plugins.
 local ensure_fo = function() vim.cmd('setlocal formatoptions-=c formatoptions-=o') end
 _G.Config.new_autocmd('FileType', '*', ensure_fo, "Proper 'formatoptions'")
+_G.Config.new_autocmd(
+  'FileType',
+  'qf',
+  function()
+    vim.keymap.set('n', '<CR>', '<CR>:cclose<CR>', { buffer = true, silent = true })
+  end,
+  "Proper 'formatoptions'"
+)
