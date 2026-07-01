25d8bb18b0bf539430b822a2885decd341fffcca
Stoyko Tolev <st.tolev@gmail.com>
2026-07-01 09:43:25 +0300

    feat(fish): Create autocomplete to auto switch all repos to main

---
+++ fish/completions/switch-to-main.fish
@@ -0,0 +1,6 @@
+# Completions for `switch-to-main` (installed at ~/.local/bin/switch-to-main).
+# Fish autoloads this file; edit here to extend.
+
+complete -c switch-to-main -s n -l dry-run           -d 'Preview changes; make none'
+complete -c switch-to-main -s u -l include-untracked -d 'Also stash untracked files'
+complete -c switch-to-main -s h -l help              -d 'Show this help'
