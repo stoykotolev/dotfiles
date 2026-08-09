2f392f4c476ce9ef8bdcc64867b0c58f5fefc975
Stoyko Tolev <st.tolev@gmail.com>
2026-08-09 16:54:55 +0300

    feat: fix keymaps for nvim surround

--- nvim/lua/plugins/editing.lua
+++ nvim/lua/plugins/editing.lua
@@ -5,10 +5,10 @@ require("mini.surround").setup({
         add = "<leader>as", -- Add surrounding in Normal and Visual modes
         delete = "<leader>ds", -- Delete surrounding
         find = "<leader>fs", -- Find surrounding (to the right)
-        find_left = "sF", -- Find surrounding (to the left)
+        find_left = "<leader>fS", -- Find surrounding (to the left)
         highlight = "<leader>hs", -- Highlight surrounding
         replace = "<leader>rs", -- Replace surrounding
-        update_n_lines = "sn", -- Update `n_lines`
+        update_n_lines = "<leader>ns", -- Update `n_lines`
         suffix_last = "l", -- Suffix to search with "prev" method
         suffix_next = "n", -- Suffix to search with "next" method
     },
